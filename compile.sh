4fdc86c394c29c17dd2c52e1b4a7ef3819b2e34c
Rikarnto Bariampa <richard1996ba@gmail.com>
2021-01-08 16:45:29 +0200

    Test Introduction
    Introductions:
    🟢 Introduction of tests to check if AMPLE was compiled successfully.
    
    Known issues:
    🔴 If a swap partition is used, memset should not be used as it requires the memory to be set to 0x00. As a result run-time speeds will suffer.
    🔴 AMPLE Assembler can not utilize labels/positions.
    🔴 AMPLE Assembler crashes if mnemonic version is not present.
    🔴 AMPLE Assembler can not parse correctly end of file.
    
    Future improvements:
    🟡 Refactor AMPLE Assembler.
    🟡 Add AMPLE Assembler arguments.
    🟡 Introduce Fail safe execution for AMPLE Assembler.
    🟡 Re introduce Core library.
    🟡 Add safe program termination.
    🟡 Introduce parallel processing and next instruction load.
    🟡 Introduce new Lower Level I/O management.
    🟡 Extend I/O Library.
    🟡 Introduce powershell script for compiling an testing AMPLE (Introduced shell for Linux and Mac OS).

--- compile.sh
+++ compile.sh
@@ -8,4 +8,11 @@ cd "build"
 
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j16
+
+cd ../tests
+if ! ./test.sh; then
+    exit $?
+fi
+cd ../build
+
 sudo make install
---
+++ tests/test1/test.asm
@@ -0,0 +1,7 @@
+mov r0, 10
+mov r1, r0
+
+add r1, r1
+mult r1, 100
+
+xor r0, r0
